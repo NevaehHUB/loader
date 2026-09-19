63e73b9cac90047c265f7b04307d4674a00c3208
Nevaeh <musixkaydo@gmail.com>
2026-09-19 04:30:26 -0700

    Create .lua

---
+++ .lua
@@ -0,0 +1,15 @@
+local config = {
+
+    [107778070777162] = "https://api.jnkie.com/api/v1/luascripts/public/ad1854a7dd4b1db76a97ef722c59b35b35aa90eed1780d9fba4aef65c8c6b2e2/download",
+
+}
+
+repeat task.wait() until game:IsLoaded()
+
+local scriptUrl = config[game.PlaceId]
+
+if scriptUrl then
+    pcall(function()
+        loadstring(game:HttpGet(scriptUrl))()
+    end)
+end

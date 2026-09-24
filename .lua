baf0cbad1f12dcd65f463a7725ae9eb76dc6983d
Nevaeh <musixkaydo@gmail.com>
2026-09-23 18:26:57 -0700

    Update .lua

--- .lua
+++ .lua
@@ -1,7 +1,7 @@
 local config = {
 
-    [16732694052] = "https://api.jnkie.com/api/v1/luascripts/public/5ad011b0e9815baf20cb915494d9bbbfa03ccb625fd76538f370491bf2bf118c/download",
-    [100539151525414] = "https://api.jnkie.com/api/v1/luascripts/public/5ad011b0e9815baf20cb915494d9bbbfa03ccb625fd76538f370491bf2bf118c/download",
+    [16732694052] = "https://api.jnkie.com/api/v1/luascripts/public/5ad011b0e9815baf20cb915494d9bbbfa03ccb625fd76538f370491bf2bf118c/download", -- Main fisch
+    [100539151525414] = "https://api.jnkie.com/api/v1/luascripts/public/5ad011b0e9815baf20cb915494d9bbbfa03ccb625fd76538f370491bf2bf118c/download", -- Fisch for new players
 
 }
 

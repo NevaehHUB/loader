2e09640643dc68deb2640032cb335604ed83cc4d
Nevaeh <musixkaydo@gmail.com>
2026-09-26 13:49:48 -0700

    Update .lua

--- .lua
+++ .lua
@@ -1,7 +1,7 @@
 local config = {
     [16732694052] = "https://api.jnkie.com/api/v1/luascripts/public/5ad011b0e9815baf20cb915494d9bbbfa03ccb625fd76538f370491bf2bf118c/download", -- Main fisch
     [131716211654599] = "https://api.jnkie.com/api/v1/luascripts/public/5ad011b0e9815baf20cb915494d9bbbfa03ccb625fd76538f370491bf2bf118c/download", -- Fisch for new players
-    [113290951185459] = "https://api.jnkie.com/api/v1/luascripts/public/62356c6cce48f2dfbc26a274f6a76e6d2b0136d4da6b69c22ee18377790a25e1/download)" -- Anime Dice
+    [113290951185459] = "https://api.jnkie.com/api/v1/luascripts/public/62356c6cce48f2dfbc26a274f6a76e6d2b0136d4da6b69c22ee18377790a25e1/download)", -- Anime Dice
 }
 
 repeat task.wait() until game:IsLoaded()
